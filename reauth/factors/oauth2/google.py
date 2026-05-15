b8a92e280e353781522bcac30d766b72b4030b75
François Voron <me@fvoron.com>
2026-05-15 14:46:36 +0200

    factors/oauth2: add Google as OIDC factor

---
+++ reauth/factors/oauth2/google.py
@@ -0,0 +1,32 @@
+import abc
+
+from .oidc import OIDCFactor
+from .state import OAuth2StateService
+
+
+class GoogleOAuth2Factor(OIDCFactor, abc.ABC):
+    """
+    Google OAuth2 factor implementation, using the OpenID Connect protocol.
+
+    References:
+        - Google: https://developers.google.com/identity/openid-connect/openid-connect
+    """
+
+    DISCOVERY_ENDPOINT = "https://accounts.google.com/.well-known/openid-configuration"
+
+    def __init__(
+        self,
+        *,
+        identifier: str = "google",
+        client_id: str,
+        state_service: OAuth2StateService,
+        client_secret: str | None = None,
+        min_prior_factors: int = 0,
+    ) -> None:
+        super().__init__(
+            identifier=identifier,
+            min_prior_factors=min_prior_factors,
+            client_id=client_id,
+            client_secret=client_secret,
+            state_service=state_service,
+        )
